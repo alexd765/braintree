6a88bdacb81b5221de95281310c1789fb36fc7dc
Alexander Döring <email@alexd.ch>
2016-12-30 17:29:49 +0100

    add more tests for plan gateway
    Fixes #9.

--- plan_test.go
+++ plan_test.go
@@ -1,16 +1,38 @@
 package braintree
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestAllPlans(t *testing.T) {
-	plans, err := bt.Plan().All()
-	if err != nil {
-		t.Fatalf("unexpected error: %s", err)
-	}
-	if size := len(plans); size != 1 {
-		t.Fatalf("expected %d plans, got %d", 1, size)
-	}
-	if plans[0].ID != "plan1" {
-		t.Errorf("expected plan.ID == plan1, got plan.ID == %s", plans[0].ID)
-	}
+	t.Parallel()
+
+	t.Run("shouldWork", func(t *testing.T) {
+		t.Parallel()
+		plans, err := bt.Plan().All()
+		if err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		}
+		if size := len(plans); size != 1 {
+			t.Fatalf("expected %d plans, got %d", 1, size)
+		}
+		if plans[0].ID != "plan1" {
+			t.Errorf("expected plan.ID == plan1, got plan.ID == %s", plans[0].ID)
+		}
+	})
+
+	t.Run("wrongMerchantID", func(t *testing.T) {
+		t.Parallel()
+		bt2 := &Braintree{
+			environment: "sandbox",
+			merchantID:  "blablub",
+			publicKey:   os.Getenv("BRAINTREE_PUB_KEY"),
+			privateKey:  os.Getenv("BRAINTREE_PRIV_KEY"),
+		}
+		bt2.planGW = PlanGW{bt: bt2}
+		if _, err := bt2.Plan().All(); err == nil || err.Error() != "401 Unauthorized" {
+			t.Errorf("got: %v, want: 401: Unauthorized", err)
+		}
+	})
 }
